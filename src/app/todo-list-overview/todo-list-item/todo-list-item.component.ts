259d087321c5f74d5d4bbace4571ed50e9663f74
WobiGe <wladimir-g@gmx.de>
2022-06-01 00:21:46 +0200

    remove add todolists

--- src/app/todo-list-overview/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list-overview/todo-list-item/todo-list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoListService } from '../todo-list.service';
+import { TodoListValues } from './todo-list-item.model';
 
 @Component({
   selector: 'app-todo-list-item',
@@ -10,18 +11,24 @@ import { TodoListService } from '../todo-list.service';
 export class TodoListItemComponent implements OnInit {
   index: number = 0;
   title: string = "TodoTitle";
-  todos: string[] = [""];
+  todos: TodoListValues[] = [];
   constructor(private todoListService: TodoListService) { }
 
   ngOnInit(): void {
   }
 
   addTodo(){
-    this.todos.push("");
+    this.todos.push(new TodoListValues(false, ""));
   }
 
-  deleteTodo(){
+  deleteTodoList(){
     this.todoListService.removeList(this.index);
+  }
+
+  removeTodo(index: number){
+    if(this.todos.length > 1){
+      this.todos.splice(index,1);
+    }
   }
 
   trackByIdx(index: number, obj: any): any {
--- src/app/todo-list-overview/todo-list-item/todo-list-item.model.ts
+++ src/app/todo-list-overview/todo-list-item/todo-list-item.model.ts
@@ -1,3 +1,8 @@
 export class TodoListItem{
-  constructor(public index: number, public title: string, public todos: string[]){}
+  constructor(public index: number, public title: string, public todos: TodoListValues[]){}
+}
+
+export class TodoListValues{
+  constructor(public checked: string|boolean, public todoString: string){
+  }
 }
